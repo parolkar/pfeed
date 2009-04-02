545ba6a12683131399e74263de93031f062135b9
Abhishek Parolkar <abhishek@parolkar.com>
2009-04-02 10:49:50 +0530

    Improvements on method injection through module

--- lib/pfeed.rb
+++ lib/pfeed.rb
@@ -7,11 +7,7 @@ module ParolkarInnovationLab
     end
     
     module ClassMethods
-      
-      def pfeed_create_block # hack from http://blog.jayfields.com/2006/07/ruby-eval-with-binding.html
-          Proc.new {}
-      end
-      
+                  
       def emits_pfeeds_for method_name_array
         include ParolkarInnovationLab::SocialNet::InstanceMethods
        
@@ -23,7 +19,7 @@ module ParolkarInnovationLab
           method_to_be_called = method + '_without_pfeed' + symbol
           eval %[
                
-             module ::ParolkarInnovationLab::SocialNet::#{self.to_s} 
+             module ::ParolkarInnovationLab::SocialNet::PfeedTemp::#{self.to_s} 
               def #{method_to_define}(*a, &b)
                 returned_result = #{method_to_be_called}(*a , &b)
                 puts "#{ParolkarInnovationLab::SocialNet::PfeedUtils.attempt_pass_tense(method)}"
@@ -31,22 +27,26 @@ module ParolkarInnovationLab
               end
              end    
           ] 
-         
-          
-          include "::ParolkarInnovationLab::SocialNet::#{self.to_s}".constantize # why this? because "define_method((method + '_with_pfeed' + symbol).to_sym) do |*a , &b|" generates syntax error in ruby < 1.8.7 
-          
-                
+            
+        }
+        
+        include "::ParolkarInnovationLab::SocialNet::PfeedTemp::#{self.to_s}".constantize # why this? because "define_method((method + '_with_pfeed' + symbol).to_sym) do |*a , &b|" generates syntax error in ruby < 1.8.7 
+        
+        method_name_array.each { |method_name|
+          method, symbol = method_name.to_s.split /(\!|\?)/
+          symbol = '' if symbol.nil?
           alias_method_chain (method + symbol), :pfeed
-          }
-       
-       
+        }
+          
       end
     end
     
+    module PfeedTemp
+      # Required for temporarily injecting new methods
+    end  
     module InstanceMethods
     
-      
-      
+            
       private
         #let private methods come here
     end
