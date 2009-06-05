7b229eb0a656d3e3ed898778b1648d47d856b806
Abhishek Parokar <abhishek@parolkar.com>
2009-06-05 12:33:46 +0530

    Created mechanism to pass AR obj references to subclass instances of PfeedItem

--- app/models/pfeed_item.rb
+++ app/models/pfeed_item.rb
@@ -7,36 +7,40 @@ class PfeedItem < ActiveRecord::Base
   belongs_to :originator, :polymorphic => true
   belongs_to :participant, :polymorphic => true
 
-  has_many :pfeed_deliveries, :dependent => :destroy 
+  has_many :pfeed_deliveries, :dependent => :destroy   
+  
+  attr_accessor :temp_references # this is an temporary Hash to hold references to temporary Objects 
   
   def self.log(ar_obj,method_name,method_name_in_past_tense,returned_result,*args_supplied_to_method,&block_supplied_to_method)
      #puts "#{ar_obj.class.to_s},#{method_name},#{method_name_in_past_tense},#{returned_result},#{args_supplied_to_method.length}"
      
-     @originator = ar_obj
-     @participant = nil
-     @participant = args_supplied_to_method[0] if args_supplied_to_method &&  args_supplied_to_method.length >= 1 && args_supplied_to_method[0].class.superclass.to_s == "ActiveRecord::Base"
-       
-     pfeed_class_name = "#{ar_obj.class.to_s.underscore}_#{method_name_in_past_tense}".camelize # may be I could use .classify
-     pfeed_class_name = "Pfeeds::"+pfeed_class_name
-     contstructor_options = { :originator_id => @originator.id , :originator_type => @originator.class.to_s , :participant_id => (@participant ? @participant.id : nil) , :participant_type => (@participant ? @participant.class.to_s : nil) } # there is a reason why I didnt use {:originator => originator} , if originator is new record it might get saved here un intentionally
-       
-       
-     p_item =  nil
-     begin
-       #puts "Attempting to create object of  #{pfeed_class_name} "
-       p_item =  pfeed_class_name.constantize.new(contstructor_options) 
-     rescue NameError
-       #puts "could not find class #{pfeed_class_name} , hence using default Pfeed"
-       p_item = PfeedItem.new(contstructor_options) 
-     end   
-     
-     p_item.pack_data(method_name,method_name_in_past_tense,returned_result,*args_supplied_to_method,&block_supplied_to_method)
-     
-     
-     p_item.save
-     #puts "Trying to deliver to #{ar_obj}  #{ar_obj.pfeed_audience_hash[method_name.to_sym]}"
-     p_item.deliver(ar_obj,ar_obj.pfeed_audience_hash[method_name.to_sym])
-     
+      temp_references = Hash.new
+      temp_references[:originator] = ar_obj
+      temp_references[:participant] = nil
+      temp_references[:participant] = args_supplied_to_method[0] if args_supplied_to_method &&  args_supplied_to_method.length >= 1 && args_supplied_to_method[0].class.superclass.to_s == "ActiveRecord::Base"
+
+      pfeed_class_name = "#{ar_obj.class.to_s.underscore}_#{method_name_in_past_tense}".camelize # may be I could use .classify
+      pfeed_class_name = "Pfeeds::"+pfeed_class_name
+      contstructor_options = { :originator_id => temp_references[:originator].id , :originator_type => temp_references[:originator].class.to_s , :participant_id => (temp_references[:participant] ? temp_references[:participant].id : nil) , :participant_type => (temp_references[:participant] ? temp_references[:participant].class.to_s : nil) } # there is a reason why I didnt use {:originator => temp_references[:originator]} , if originator is new record it might get saved here un intentionally
+
+
+      p_item =  nil
+      begin
+        #puts "Attempting to create object of  #{pfeed_class_name} "
+        p_item =  pfeed_class_name.constantize.new(contstructor_options) 
+        p_item.temp_references =  temp_references
+      rescue NameError
+        #puts "could not find class #{pfeed_class_name} , hence using default Pfeed"
+        p_item = PfeedItem.new(contstructor_options) 
+      end   
+
+      p_item.pack_data(method_name,method_name_in_past_tense,returned_result,*args_supplied_to_method,&block_supplied_to_method)
+
+
+      p_item.save
+      #puts "Trying to deliver to #{ar_obj}  #{ar_obj.pfeed_audience_hash[method_name.to_sym]}"
+      p_item.deliver(ar_obj,ar_obj.pfeed_audience_hash[method_name.to_sym])
+               
   end  
 
   def deliver(ar_obj,method_name_arr)
